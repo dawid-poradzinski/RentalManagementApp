91fbd33a7d133ba3c9e8649aa67688278ece8343
dawid <dawid.poradzinski@gmail.com>
2025-12-19 14:12:41 +0100

    working groovy

---
+++ Backend/src/main/java/com/dawid/poradzinski/school/ski_rent_app/controller/ItemController.java
@@ -0,0 +1,11 @@
+package com.dawid.poradzinski.school.ski_rent_app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("items")
+public class ItemController {
+    
+    
+}
